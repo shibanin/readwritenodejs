65cb99ebcc829d5eefcbc1cdbd707fd3078fdce0
shibanin <shibanin@gmail.com>
2021-12-30 12:04:36 -0800

    Update Read_favcolor_name.js

--- Read_favcolor_name.js
+++ Read_favcolor_name.js
@@ -1,4 +1,4 @@
-
+//NodeJS "readline" API used for recording inputs from user
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-
+//Requesting multiple user inputs: name, color
 rl. question('What is your name?', (name)=> {
     rl. question('What is your favorite color?', (color)=> {
     console.log(`Thanks you for your response: ${name}, recorded ${color} as your favorite color!`);
