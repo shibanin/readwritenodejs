46c6bd179f0109943f8720c4578ee0f1a6246fc4
shibanin <shibanin@gmail.com>
2021-12-30 16:13:32 -0800

    Update writefile.js

--- writefile.js
+++ writefile.js
@@ -1,6 +1,5 @@
-//NodeJS code to implement read API "readline" and write/filesystem API "fs"
-//to create and write into a file, accepting user input
-
+// NodeJS code to implement read API "readline" and write/filesystem API "fs"
+// to create and write into a file, accepting user input
 
 // Incorporate read and write API
 const readline = require('readline');
@@ -15,10 +14,9 @@ rl.question('What is your name?', (name) => {
     console.log(`Thanks you for your response: ${name}, it will be recorded in "greeting.txt" file`);
     rl.close();
     write_to_file(name);
-
 });
 
-//function write_to_file which uses "filesystem" API to create and write into a file "greeting.txt"
+// function write_to_file which uses "filesystem" API to create and write into a file "greeting.txt"
 const write_to_file = (name) => {
     fs.writeFile('greeting.txt', `Hello, ${name}!`, err => {
         if (err) {
